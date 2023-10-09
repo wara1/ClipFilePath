09e8a61c0c16fb4dc97f43fd8b43263842bec841
Keita Fujiwara <key.micro20@outlook.jp>
2023-10-10 02:47:26 +0900

    一応完成

--- src/func/clip_file_path.cpp
+++ src/func/clip_file_path.cpp
@@ -1,5 +1,7 @@
-#include "clip_file_path.hpp"
+#include <windows.h>
+#include <algorithm>
 
+#include "clip_file_path.hpp"
 
 // ---------------------------------------------
 // Public
@@ -12,22 +14,91 @@ ClipFilePath::ClipFilePath(const std::vector<std::string>& command_line_args)
 
 ClipFilePath::~ClipFilePath()
 {
-
 }
 
 ClipFilePath::Resutl ClipFilePath::Clipping() const
 {
-  return ClipFilePath::Resutl::OK;
+  Resutl ret = Resutl::NG;
+
+  std::string clipping_text;
+
+  std::unordered_set<std::string> keys = ExtractKeys_(file_paths_);
+  for (const auto& key : keys)
+  {
+    clipping_text += "<" + key + ">" + K_NEW_LINE_CODE;
+
+    const auto key_iterators = file_paths_.equal_range(key);
+
+    std::for_each(key_iterators.first, key_iterators.second, [&clipping_text](std::pair<std::string, std::string> map_data) {
+        clipping_text += "・" + map_data.second + K_NEW_LINE_CODE;
+      }
+    );
+  }
+
+  if (SetClipboardText_(clipping_text))
+  {
+    ret = Resutl::OK;
+  }
+
+  return ret;
 }
 
 // ---------------------------------------------
 // Private
 // ---------------------------------------------
 
-std::map<std::string, std::vector<std::string>> ClipFilePath::AnalyseCommandLineArgs_(
+std::unordered_multimap<std::string, std::string> ClipFilePath::AnalyseCommandLineArgs_(
   const std::vector<std::string>& command_line_args)
 {
-  std::map<std::string, std::vector<std::string>> file_paths;
+  std::unordered_multimap<std::string, std::string> file_paths;
+
+  if (1U < command_line_args.size())
+  {
+    // ソート
+    std::vector<std::string> temp_file_paths(++command_line_args.begin(), command_line_args.end());
+    std::sort(temp_file_paths.begin(), temp_file_paths.end());
+
+    // パスとファイル名分離/設定
+    for (const auto& file_path : temp_file_paths)
+    {
+      std::string directory;
+      std::string fine_name;
+      
+      const size_t found = file_path.find_last_of("/\\");   // 最後のスラッシュまたはバックスラッシュを探して、パスを分割する
+      if (found != std::string::npos) {
+        directory = file_path.substr(0, found);
+        fine_name = file_path.substr(found + 1);
+
+        file_paths.emplace(directory, fine_name);           // 結果へ格納
+      }
+    }
+  }
 
   return file_paths;
+}
+
+bool ClipFilePath::SetClipboardText_(const std::string& text) const
+{
+  bool ret = false;
+
+  if ((!text.empty()) && OpenClipboard(nullptr)) {
+    // クリップボードをクリア
+    EmptyClipboard();
+
+    // 文字列をクリップボードに設定
+    HGLOBAL hClipboardData = GlobalAlloc(GMEM_MOVEABLE, (text.length() + 1) * sizeof(char));
+    if (hClipboardData != nullptr) {
+      char* pchData = static_cast<char*>(GlobalLock(hClipboardData));
+      if (pchData != nullptr) {
+        strcpy_s(pchData, text.length() + 1, text.c_str());
+        GlobalUnlock(hClipboardData);
+        SetClipboardData(CF_TEXT, hClipboardData);
+      }
+    }
+
+    CloseClipboard();
+    ret = true;
+  }
+
+  return ret;
 }
--- src/func/clip_file_path.hpp
+++ src/func/clip_file_path.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <string>
 
 /// @brief クリップファイルパス 機能クラス
@@ -11,8 +12,8 @@ class ClipFilePath
   /// @brief 結果
   enum class Resutl
   {
-    OK,       //!< 成功
-    ERROR,    //!< 失敗
+    OK,        //!< 成功
+    NG,        //!< 失敗
   };
 
   /// @brief コンストラクタ
@@ -29,10 +30,35 @@ class ClipFilePath
   Resutl Clipping() const;
 
  private:
+  static constexpr const char* const K_NEW_LINE_CODE = "\r\n";
+
   /// @brief コマンドライン引数解析
   /// @param[in] command_line_args コマンドライン引数
   /// @return ファイルパス
-  std::map<std::string, std::vector<std::string>> AnalyseCommandLineArgs_(const std::vector<std::string>& command_line_args);
+  std::unordered_multimap<std::string, std::string> AnalyseCommandLineArgs_(const std::vector<std::string>& command_line_args);
 
-  const std::map<std::string, std::vector<std::string>> file_paths_;   //!< ファイルパス(パス,ファイル名[複数]の連想配列)
+  /// @brief クリップボードテキスト設定
+  /// @param[in] text 設定するテキスト
+  /// @return 結果
+  /// @retval true 設定成功
+  /// @retval false 設定失敗
+  bool SetClipboardText_(const std::string& text) const;
+
+  /// @briefテンプレート関数で任意のunordered_multimap型に対応
+  /// @param[in] inputMap キー抽出対象の連想配列
+  /// @return 重複なしキー値
+  template <typename KeyType, typename ValueType>
+  std::unordered_set<KeyType> ExtractKeys_(const std::unordered_multimap<KeyType, ValueType>& inputMap) const
+  {
+    std::unordered_set<KeyType> keys;
+
+    // unordered_multimapの要素をイテレートし、キーをkeysに追加
+    for (const auto& pair : inputMap) {
+      keys.insert(pair.first);
+    }
+
+    return keys;
+  }
+
+  const std::unordered_multimap<std::string, std::string> file_paths_;   //!< ファイルパス(パス,ファイル名[複数]の連想配列)
 };
